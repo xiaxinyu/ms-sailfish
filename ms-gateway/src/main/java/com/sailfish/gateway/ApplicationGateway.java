63ca6d0ef7fbe68f001afc610167c13e4de7a12e
summer.xia <summer_west2010@126.com>
2019-04-29 22:56:26 +0800

    create gateway

---
+++ ms-gateway/src/main/java/com/sailfish/gateway/ApplicationGateway.java
@@ -0,0 +1,17 @@
+package com.sailfish.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ApplicationGateway {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGateway.class, args);
+	}
+}
