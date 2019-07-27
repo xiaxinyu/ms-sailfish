a05f6eb1377145d8eb1ca9e761f4c05c470e29ab
xiaxinyu <summer_west2010@126.com>
2019-07-27 16:45:17 +0800

    test string

---
+++ ms-utils/src/test/java/com.sailfish.test/StringTest.java
@@ -0,0 +1,18 @@
+package com.sailfish.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringTest {
+
+    @Test
+    public void testString() {
+        String t1 = new String("123");
+        String t2 = t1.intern();
+        String t3 = "123";
+
+        Assert.assertFalse(t1 == t2);
+        Assert.assertFalse(t1 == t3);
+        Assert.assertTrue(t2 == t3);
+    }
+}
