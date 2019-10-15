3adfc103982a66425d489781be8482f1a74894bd
xiaxinyu <summer_west2010@126.com>
2019-10-15 13:52:47 +0800

    optimize register center

--- ms-register-center/src/main/java/com/sailfish/register/center/ApplicationRegisterCenter.java
+++ ms-register-center/src/main/java/com/sailfish/register/center/ApplicationRegisterCenter.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ApplicaitonEureka {
+public class ApplicationRegisterCenter {
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicaitonEureka.class, args);
+		SpringApplication.run(ApplicationRegisterCenter.class, args);
 	}
 }
