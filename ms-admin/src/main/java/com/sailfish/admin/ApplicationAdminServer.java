454a6867e315a96c0b0718e86af1d02027b3ecc4
xiaxinyu3 <xiaxinyu3@crc.com.hk>
2019-11-27 15:09:49 +0800

    add dockerfile for ms-admin

--- ms-admin/src/main/java/com/sailfish/admin/ApplicationAdminServer.java
+++ ms-admin/src/main/java/com/sailfish/admin/ApplicationAdminServer.java
@@ -3,11 +3,9 @@ package com.sailfish.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAdminServer
 @SpringBootApplication
-@EnableEurekaClient
 public class ApplicationAdminServer {
 
     public static void main(String[] args) {
