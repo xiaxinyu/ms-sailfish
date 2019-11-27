8bb2f802893661dbefeea2345f74aeddab045676
xiaxinyu3 <xiaxinyu3@crc.com.hk>
2019-11-27 11:44:04 +0800

    add spring admin

---
+++ ms-admin/src/main/java/com/sailfish/admin/ApplicationAdminServer.java
@@ -0,0 +1,17 @@
+package com.sailfish.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableAdminServer
+@SpringBootApplication
+@EnableEurekaClient
+public class ApplicationAdminServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationAdminServer.class, args);
+    }
+
+}
