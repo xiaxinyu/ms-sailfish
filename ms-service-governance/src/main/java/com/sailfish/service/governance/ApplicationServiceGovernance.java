71ccad28cecf95484a848206157d231de291825c
xiaxinyu3 <xiaxinyu3@crc.com.hk>
2019-11-29 17:04:15 +0800

    add service governance

---
+++ ms-service-governance/src/main/java/com/sailfish/service/governance/ApplicationServiceGovernance.java
@@ -0,0 +1,11 @@
+package com.sailfish.service.governance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationServiceGovernance {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationServiceGovernance.class, args);
+    }
+}
